ba97e2b3f1a73e3bb841916f4599d7ad1b981580
sasagram <86244926+sasagram@users.noreply.github.com>
2021-09-26 19:29:31 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+'use strict'
+
+{
+  function callback(entries, obs) {
+    entries.forEach(entry =>{
+      if (!entry.isIntersecting) {
+        return;
+      }
+      entry.target.classList.add('appear');
+      obs.unobserve(entry.target);
+    });
+  }
+
+  const options = {
+    threshold:0.2,
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  const  targets = document.querySelectorAll('.animate');
+
+  targets.forEach(target => {
+    observer.observe(target);
+  });
+  
+}
